d57154aa76514db3ad716f35fda391f5c73186d0
TjarkN <github@tja-rk.de>
2026-07-07 16:49:00 +0200

    Added Security with Username and Passwords

--- src/process_mining_api/llm_response.py
+++ src/process_mining_api/llm_response.py
@@ -8,8 +8,10 @@ class QueryHandler():
     def __init__(self, collection_name = None, qdrant_url ="http://localhost:6333/"):
         if collection_name is None:
             collection_name = os.getenv("COLLECTION_NAME")
-        self.__retriever_controller = RetrievalController(collection_name,qdrant_url)
-
+        try:
+            self.__retriever_controller = RetrievalController(collection_name,qdrant_url)
+        except Exception as e:
+            print(e)
 
     def simple_query(self, query: str, ):
         response = self.__retriever_controller.simple_query_json(query)
